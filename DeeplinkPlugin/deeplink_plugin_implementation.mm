c977dc4d16e4805b6efc7b81d1db5367e131092e
Cengiz <cengiz@puzly.co>
2024-12-21 16:07:58 +0600

    Fixed deeplink notification at startup.

--- DeeplinkPlugin/deeplink_plugin_implementation.mm
+++ DeeplinkPlugin/deeplink_plugin_implementation.mm
@@ -3,6 +3,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #import "deeplink_plugin_implementation.h"
 #import "deeplink_service.h"
@@ -12,6 +13,7 @@
 String const DEEPLINK_RECEIVED_SIGNAL = "deeplink_received";
 
 DeeplinkPlugin* DeeplinkPlugin::instance = NULL;
+NSURL* DeeplinkPlugin::receivedUrl;
 
 
 void DeeplinkPlugin::_bind_methods() {
@@ -33,15 +35,20 @@ Error DeeplinkPlugin::initialize() {
 		return FAILED;
 	}
 
+	if (receivedUrl) {
+		emit_signal(DEEPLINK_RECEIVED_SIGNAL, [GDPConverter nsUrlToGodotDictionary:receivedUrl]);
+	}
+
 	initialized = true;
+
 	return OK;
 }
 
 String DeeplinkPlugin::get_url() {
 	String result = "";
 
-	if (this->receivedUrl) {
-		result = [GDPConverter nsStringToGodotString:this->receivedUrl.absoluteString];
+	if (receivedUrl) {
+		result = [GDPConverter nsStringToGodotString:receivedUrl.absoluteString];
 	}
 
 	return result;
@@ -50,8 +57,8 @@ String DeeplinkPlugin::get_url() {
 String DeeplinkPlugin::get_scheme() {
 	String result = "";
 
-	if (this->receivedUrl) {
-		result = [GDPConverter nsStringToGodotString:this->receivedUrl.scheme];
+	if (receivedUrl) {
+		result = [GDPConverter nsStringToGodotString:receivedUrl.scheme];
 	}
 
 	return result;
@@ -60,8 +67,8 @@ String DeeplinkPlugin::get_scheme() {
 String DeeplinkPlugin::get_host() {
 	String result = "";
 
-	if (this->receivedUrl) {
-		result = [GDPConverter nsStringToGodotString:this->receivedUrl.host];
+	if (receivedUrl) {
+		result = [GDPConverter nsStringToGodotString:receivedUrl.host];
 	}
 
 	return result;
@@ -70,19 +77,36 @@ String DeeplinkPlugin::get_host() {
 String DeeplinkPlugin::get_path() {
 	String result = "";
 
-	if (this->receivedUrl) {
-		result = [GDPConverter nsStringToGodotString:this->receivedUrl.path];
+	if (receivedUrl) {
+		result = [GDPConverter nsStringToGodotString:receivedUrl.path];
 	}
 
 	return result;
 }
 
 void DeeplinkPlugin::clear_data() {
-	this->receivedUrl = NULL;
+	receivedUrl = NULL;
 }
 
-void DeeplinkPlugin::set_received_url(NSURL* url) {
-	this->receivedUrl = url;
+void DeeplinkPlugin::navigate_to_open_by_default_settings() {
+	// if (@available(iOS 18.3, *)) {
+	// 	// Create the URL that deep links to your app's custom settings.
+	// 	NSURL *url = [[NSURL alloc] initWithString:UIApplicationOpenDefaultApplicationsSettingsURLString];
+	// 	// Ask the system to open that URL.
+	// 	[[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+	// }
+	// else {
+	// 	NSLog(@"DeeplinkPlugin::navigate_to_open_by_default_settings: ERROR: iOS version 18.3 or greater is required!");
+	// }
+	if (@available(iOS 8.0, *)) {
+		// Create the URL that deep links to your app's custom settings.
+		NSURL *url = [[NSURL alloc] initWithString:UIApplicationOpenSettingsURLString];
+		// Ask the system to open that URL.
+		[[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+	}
+	else {
+		NSLog(@"DeeplinkPlugin::navigate_to_open_by_default_settings: ERROR: iOS version 8.0 or greater is required!");
+	}
 }
 
 DeeplinkPlugin* DeeplinkPlugin::get_singleton() {
@@ -92,16 +116,15 @@ DeeplinkPlugin* DeeplinkPlugin::get_singleton() {
 DeeplinkPlugin::DeeplinkPlugin() {
 	NSLog(@"DeeplinkPlugin constructor");
 
-	[GodotApplicalitionDelegate addService:[DeeplinkService alloc]];
-
 	ERR_FAIL_COND(instance != NULL);
 	instance = this;
 
-	initialized = true;
+	initialized = false;
 }
 
 DeeplinkPlugin::~DeeplinkPlugin() {
 	NSLog(@"DeeplinkPlugin destructor");
+
 	if (instance == this) {
 		instance = NULL;
 	}
